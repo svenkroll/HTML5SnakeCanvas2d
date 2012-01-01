7d865025d01e2ef81f0eca6eebbf27500b83907b
Sven Kroll <sk@sevenload.com>
2012-01-01 21:56:59 +0100

    Beta stage, added menue container and scoring

--- snake.js
+++ snake.js
@@ -1,37 +1,24 @@
 function checkSupported() {
     canvas = document.getElementById('canvas');
+    canvas_menue = document.getElementById('canvas_menue');
+    
     if (canvas.getContext){
-      	ctx = canvas.getContext('2d');  
+      	ctx = canvas.getContext('2d');
+        ctx_m = canvas_menue.getContext('2d');
       	// Canvas is supported
        	// This sets the fill color to red
- 		ctx.fillStyle = "rgb(200,0,0)"; 
- 		// This sets some variables for demonstration purposes
- 		var x = 50;
- 		var y = 50;
- 		var width = 10;
- 		var height = 10;
- 		snakeBody = [];
- 		snakeLength = 3;
- 		foodItems = 0;
-                maxFoodItems = 1;
-		// The current position of the Snake's head, as xy coordinates
-		this.currentPosition = {'x':50, 'y':50}; 
-		direction = 'right';
-                
-		setInterval(snakeLoop,100);
-
-		// Sets the grid dimensions as one value
-		this.gridSize = 10;
-		
-		document.onkeydown = keyPressed;
-
+        ctx.fillStyle = "rgb(200,0,0)";  
+        ctx_m.fillStyle = "rgb(200,0,0)";  
+        this.gridSize = 10;
+        start();
     } else {
       // Canvas is not supported
       alert("We're sorry, but your browser does not support the canvas tag. Please use any web browser other than Internet Explorer.");
     }
  }
 
-function makeFoodItem(){
+function makeFoodItem()
+{
    suggestedPoint = [Math.floor(Math.random()*(canvas.width/gridSize))*gridSize, Math.floor(Math.random()*(canvas.height/gridSize))*gridSize];
    if (snakeBody.some(hasPoint)) {
      makeFoodItem();
@@ -49,7 +36,7 @@ function hasPoint(element, index, array)
 
 
 function moveUp(){
- 	if ((currentPosition['y'] - gridSize) >= 0) {
+    if ((currentPosition['y'] - gridSize) >= 0) {
      executeMove('up', 'y', (currentPosition['y'] - gridSize));
    } else {
      whichWayToGo('x');
@@ -87,6 +74,7 @@ function executeMove(dirValue, axisType, axisValue) {
 }
 
 function snakeLoop(){
+   updateScore();
    if (foodItems < maxFoodItems)
    {
        makeFoodItem();
@@ -109,7 +97,15 @@ function snakeLoop(){
        moveRight();
        break;
    }
+   drawMenue();
 }// End snakeLoop
+
+function drawMenue()
+{
+    ctx_m.font = "18pt Arial"; 
+    ctx_m.clearRect(0,0, canvas_menue.width, canvas_menue.height);
+    ctx_m.fillText("Score: " + score, 10, 20); 
+}
 
 function whichWayToGo(axisType){  
   if (axisType=='x') {
@@ -119,7 +115,13 @@ function whichWayToGo(axisType){
   }
 }
 
-function drawSnake() {
+function drawSnake() 
+{
+        if (snakeBody.some(hasEatenItself)) 
+        {
+            gameOver();
+            return;
+        }
 	snakeBody.push([currentPosition['x'], currentPosition['y']]);
    	ctx.fillRect(currentPosition['x'], currentPosition['y'], gridSize, gridSize);
    	if (snakeBody.length > snakeLength) {
@@ -129,45 +131,112 @@ function drawSnake() {
         if (currentPosition['x'] == suggestedPoint[0] && currentPosition['y'] == suggestedPoint[1]) {
             makeFoodItem();
             snakeLength += 1;
-        }
+        }       
 }
- 
+
+function hasEatenItself(element, index, array) 
+{
+    return (element[0] == currentPosition['x'] && element[1] == currentPosition['y']);  
+}
+
+function updateScore(){
+   score = (snakeLength - 3)*10
+   //document.getElementById('score').innerText = score;
+}
+
+function gameOver()
+{
+    updateScore();
+    pause();
+    foodItems = 0;
+    snakeBody = [];
+    snakeLength = 3;
+    alert("Game Over. Your score was "+ score);
+    ctx.clearRect(0,0, canvas.width, canvas.height);
+    document.getElementById('play_menu').style.display='none';
+    document.getElementById('restart_menu').style.display='block';
+}
+
+function pause(){
+    clearInterval(interval);
+    allowPressKeys = false;
+}
+
+function play(){
+    updateScore();
+    interval = setInterval(snakeLoop,100);
+    allowPressKeys = true;
+}
+
+function start(){
+    document.onkeydown = keyPressed;
+    ctx.clearRect(0,0, canvas.width, canvas.height);
+    snakeBody = [];
+    snakeLength = 3;
+    foodItems = 0;
+    maxFoodItems = 1;
+    // The current position of the Snake's head, as xy coordinates
+    this.currentPosition = {'x':50, 'y':50}; 
+    direction = 'right';
+    play();  
+}
+
+function restart(){
+    pause();
+    start();
+}
+
 function keyPressed(event)
 {
-    		var keyCode; 
-    		if(!event)
-    		{
-      			keyCode = window.event.keyCode; 
-    		}
-    		else 
-    		{
-     			keyCode = event.keyCode; 
-   			}
+    if (!allowPressKeys){
+        return;
+    }
+    var keyCode; 
+    if(!event)
+    {
+            keyCode = window.event.keyCode; 
+    }
+    else 
+    {
+        keyCode = event.keyCode; 
+    }
  
-    		switch(keyCode)
-   			{
-			     // left 
-			     case 37:
-			     	// action when pressing left key.
-			     	direction = 'left';
-					break; 
-			 
-			     // up 
-			     case 38:
-			     	// action when pressing up key
-			     	direction = 'up';	       	
-					break; 
-			     // right 
-			     case 39:	
-			     	// action when pressing right key
-			       	direction = 'right';	       
-					break;		 
-			     // down
-			     case 40:
-			     	// action when pressing down key
-			     	direction = 'down';
-			       	break; 		 
-			     default:
-			       break; 
-			  } //End switch
+    switch(keyCode)
+    {
+         // left 
+         case 37:
+            // action when pressing left key.
+            if ( direction != 'right')
+                {
+                    direction = 'left';
+                }
+            break; 
+
+         // up 
+         case 38:
+            // action when pressing up key
+            if ( direction != 'down')
+                {
+                    direction = 'up';	       	
+                }
+            break; 
+         // right 
+         case 39:	
+            // action when pressing right key
+            if ( direction != 'left')
+                {
+                    direction = 'right';
+                }
+            break;		 
+         // down
+         case 40:
+            // action when pressing down key
+            if ( direction != 'up')
+                {
+                    direction = 'down';
+                }
+            break; 		 
+         default:
+           break; 
+      } //End switch
 }//End onkeydown
